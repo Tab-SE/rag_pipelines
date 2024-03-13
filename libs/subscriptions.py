eb8d69f71fac08007997d762f269eb220be3b893
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-13 00:16:39 -0500

    subscriptions library will handle all requests until metrics are formed

--- libs/subscriptions.py
+++ libs/subscriptions.py
@@ -5,17 +5,21 @@ from utils.http import httpGet
 def metrics(domain, credentials):
     token = credentials['credentials']['token']
     user = credentials['credentials']['user']['id']
-    endpoint = f"{domain}/api/-/pulse/subscriptions?user_id={user}" 
 
-    print('endpoint', endpoint)
-
+    # shared by all Pulse endpoints
+    path = f"{domain}/api/-/pulse" 
     headers = {
       'Content-Type': 'application/json',
       'Accept': 'application/json',
       'X-Tableau-Auth': token
     }
 
+    subscriptions = getSubscriptions(path=path, headers=headers, user=user)
+    
+    return subscriptions
+
+def getSubscriptions(path, headers, user):
+    endpoint =  path + f"/subscriptions?user_id={user}"
     response = httpGet(endpoint=endpoint, headers=headers)
     body = json.loads(response.text)
-    
     return body
--- scripts/query.py
+++ scripts/query.py
@@ -3,11 +3,11 @@ import pandas as pd
 from libs import session, subscriptions, bundles
 
 def query(env_vars):
-  DOMAIN, API, SITE, CLIENT_ID, SECRET, SECRET_ID, USER = env_vars.values()
+  domain = env_vars['DOMAIN']
   credentials = session.authenticate(env_vars)
   print('credentials', credentials)
 
-  metrics = subscriptions.metrics(domain=DOMAIN, credentials=credentials)
+  metrics = subscriptions.metrics(domain=domain, credentials=credentials)
   print('metrics', metrics)
 
   # insights = bundles(credentials, metrics)
