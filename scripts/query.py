8db8bbd579310774551db1a8dd0275cdb5d60da3
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-13 23:22:43 -0500

    removed prints

--- scripts/query.py
+++ scripts/query.py
@@ -2,18 +2,15 @@ from libs import bundles, session, subscriptions
 
 def query(env_vars):
     domain = env_vars['DOMAIN']
+
     credentials = session.authenticate(env_vars)
-    # print('credentials', credentials)
 
     metrics = subscriptions.metrics(domain=domain, credentials=credentials)
-    # print('metrics', metrics)
 
     insights = bundles.insights(domain=domain, credentials=credentials, metrics=metrics)
-    # print('insights', insights)
 
     for key, value in insights.items():
-        print(f"Key: {key}, Value: {value}")
+        # print(f"Key: {key}, Value: {value}")
         pass
 
     return insights
-
