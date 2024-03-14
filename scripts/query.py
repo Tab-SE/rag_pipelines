2c09499d7d56f741956cacb1a01836209ba0ae7d
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-13 22:26:12 -0500

    printing each metric bundle

--- scripts/query.py
+++ scripts/query.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from libs import session, subscriptions, bundles
+from libs import bundles, session, subscriptions
 
 def query(env_vars):
     domain = env_vars['DOMAIN']
@@ -8,11 +8,14 @@ def query(env_vars):
     # print('credentials', credentials)
 
     metrics = subscriptions.metrics(domain=domain, credentials=credentials)
-    print('metrics', metrics)
+    # print('metrics', metrics)
 
-    # insights = bundles(credentials, metrics)
+    insights = bundles.insights(domain=domain, credentials=credentials, metrics=metrics)
     # print('insights', insights)
 
-    insights = metrics
+    for key, value in insights.items():
+        print(f"Key: {key}, Value: {value}")
+        pass
 
     return insights
+
