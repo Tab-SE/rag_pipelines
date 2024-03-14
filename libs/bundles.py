1aac7e468db0dbdf47abcec3fe52dbd85cb71064
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-13 22:25:42 -0500

    generating insights from /detail

--- libs/bundles.py
+++ libs/bundles.py
@@ -1,3 +1,67 @@
+from datetime import datetime
+from tzlocal import get_localzone
 
-def getInsights():
-    return
+from utils.http import httpPost
+
+def insights(domain, credentials, metrics):
+    insights_bundles = {}
+    # REST API authentication token
+    token = credentials['credentials']['token']
+    # generates full set of insights from Pulse
+    endpoint = f"{domain}/api/-/pulse/insights/detail" 
+    # carries authentication token and sets response format
+    headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    'X-Tableau-Auth': token
+    }
+
+    # provide common time definitions for all insights requests
+    current_datetime = datetime.now()
+    # formatted as "YYYY-MM-DD HH:mm:ss"
+    formatted_time = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+    # Get the local timezone object
+    local_timezone = get_localzone()
+    # Get the IANA timezone name
+    timezone_name = current_datetime.astimezone(local_timezone).tzinfo.zone
+    time_options = {
+        'formatted_time': formatted_time,
+        'timezone_name': timezone_name
+    }
+
+    for key, metric in metrics.items():
+        insights = queryInsights(endpoint=endpoint, headers=headers, metric=metric, time_options=time_options)
+        insights_bundles[key] = insights
+    
+    return insights_bundles
+
+def queryInsights(endpoint, headers, metric, time_options):
+    # request body needed to generate insights
+    payload = {
+        "bundle_request": {
+            "version": "1",
+            "options": {
+                "output_format": "OUTPUT_FORMAT_TEXT",
+                "now": time_options['formatted_time'],
+                "time_zone": time_options['timezone_name']
+            },
+            "input": {
+                "metadata": {
+                    "name": metric['name'],
+                    "metric_id": metric['id'],
+                    "definition_id": metric['definition_id']
+                },
+                "metric": {
+                    "definition": metric['definition'],
+                    "metric_specification": metric['specification'],
+                    "extension_options": metric['extension_options'],
+                    "representation_options": metric['representation_options'],
+                    "insights_options": metric['insights_options']
+                }
+            }
+        }
+    }
+    
+    response = httpPost(endpoint=endpoint, headers=headers, payload=payload)
+    body = response['body']
+    return body
