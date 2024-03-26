41fb9fadb36d20ee11688f82e04d1b1939ceab7a
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-03-25 23:25:40 -0500

    creating separate vectorizations per use case

--- libs/vectorize.py
+++ libs/vectorize.py
@@ -7,32 +7,94 @@ from pinecone import Pinecone
 
 from libs import clean
 
+
+def load_monolith():
+    directory_path = 'data/'
+    documents = gather_documents(directory_path)
+    # construct pinecone client for target index
+    index = build_index('PINECONE_INDEX_NAME')
+    # build vector store, index and upsert to Pinecone
+    vectorize(index=index, documents=documents)
+    return True
+
+def load_corpus():
+    directory_path = 'data/' + 'corpus/'
+    documents = gather_documents(directory_path)
+    # construct pinecone client for target index
+    index = build_index('CORPUS_INDEX')
+    # build vector store, index and upsert to Pinecone
+    vectorize(index=index, documents=documents)
+    return True
+
 def load_insights():
-    # directory path
-    directory_path = 'data/insights/'
+    directory_path = 'data/' + 'insights/'
+    documents = gather_documents(directory_path)
+    # construct pinecone client for target index
+    index = build_index('INSIGHTS_INDEX')
+    # build vector store, index and upsert to Pinecone
+    vectorize(index=index, documents=documents)
+    return True
+
+def load_headlessbi():
+    directory_path = 'data/' + 'headlessbi/'
+    documents = gather_documents(directory_path)
+    # construct pinecone client for target index
+    index = build_index('HEADLESS_BI_INDEX')
+    # build vector store, index and upsert to Pinecone
+    vectorize(index=index, documents=documents)
+    return True
+
+def load_writer():
+    directory_path = 'data/' + 'literature/'
+    documents = gather_documents(directory_path)
+    # construct pinecone client for target index
+    index = build_index('WRITING_INDEX')
+    # build vector store, index and upsert to Pinecone
+    vectorize(index=index, documents=documents)
+    return True
+
+def gather_documents(path):
+    print('path', path)
     # Create an instance of SimpleDirectoryReader
     reader = SimpleDirectoryReader(
-        input_dir=directory_path,
-        recursive=True
+        input_dir=path,
+        recursive=True,
+        exclude_hidden=True
     )
+    # monolith must exclude literature data used to generate the corpus
+    if path == 'data/':
+        reader = SimpleDirectoryReader(
+            input_dir=path,
+            recursive=True,
+            exclude_hidden=True,
+            exclude='data/literature/'
+        )
     # Load the documents from the directory
     documents = reader.load_data()
+    # remove emaining \n characters and broken, hyphenated words
+    cleaned_docs = clean_docs(documents)
+    return cleaned_docs
+
+def clean_docs(documents):
     # remove emaining \n characters and broken, hyphenated words
     cleaned_docs = []
     for d in documents:
         cleaned_text = clean.clean_up_text(d.text)
         d.text = cleaned_text
         cleaned_docs.append(d)
+    return cleaned_docs
 
+def build_index(pinecone_index):
     pinecone_api = os.environ['PINECONE_API_KEY']
     pinecone_environment = os.environ['PINECONE_ENVIRONMENT']
-    pinecone_index = os.environ['PINECONE_INDEX_NAME']
-
+    pinecone_index = os.environ[pinecone_index]
     # initialize pinecone client
     pc = Pinecone(api_key=pinecone_api, environment=pinecone_environment)
     # client instance targets declared index
     index = pc.Index(pinecone_index)
+    return index
 
+def vectorize(index, documents):
     # construct vector store
     vector_store = PineconeVectorStore(pinecone_index=index)
     # specifies location, environment and index for storage
@@ -41,5 +103,3 @@ def load_insights():
     index = VectorStoreIndex.from_documents(
         documents, storage_context=storage_context
     )
-
-    return True
--- scripts/load.py
+++ scripts/load.py
@@ -6,6 +6,16 @@ def data(bundles):
     print('Storing data to file system...')
     insights_stored = store.insights_corpus(insights)
     print('Loading data for indexing...')
-    is_loaded = vectorize.load_insights()
+    monolith_loaded = vectorize.load_monolith()
+    corpus_loaded = vectorize.load_corpus()
+    insights_loaded = vectorize.load_insights()
+    headlessbi_loaded = False
+    writer_loaded = vectorize.load_writer()
     print('Data upserted to vector index successfully!')
-    return insights
+    return {
+        'monolith_loaded':  monolith_loaded,
+        'corpus_loaded':  corpus_loaded,
+        'insights_loaded':  insights_loaded,
+        'headlessbi_loaded':  headlessbi_loaded,
+        'writer_loaded':  writer_loaded
+    }
