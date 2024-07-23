16692ffb7df4956b68e9a6e24c95947fd07ec7f8
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-22 23:16:07 -0600

    reusable requests

--- utils/gql.py
+++ utils/gql.py
@@ -1,169 +1,18 @@
-import aiohttp
-import json
+import os, json, aiohttp
 
-async def query():
-    url = "https://prod-useast-b.online.tableau.com/api/metadata/graphql"
+async def query(query, token):
+    domain = os.environ('TABLEAU_DOMAIN')
+    url = f"{domain}/api/metadata/graphql"
 
     payload = json.dumps({
-        "query": """query Content {
-  workbooks {
-    id
-    luid
-    name
-    description
-    createdAt
-    projectName
-    projectVizportalUrlId
-    projectLuid
-    uri
-    tags {
-      id
-      name
-    }
-    dashboards {
-      id
-      luid
-    }
-    sheets {
-      id
-      luid
-    }
-  }
-
-  dashboards {
-    id
-    luid
-    name
-    path
-    createdAt
-    updatedAt
-    index
-    tags {
-      id
-      name
-    }
-    sheets {
-      id
-      luid
-    }
-  }
-
-  sheets {
-    id
-    luid
-    documentViewId
-    name
-    path
-    createdAt
-    updatedAt
-    index
-    tags {
-      id
-      name
-    }
-    containedInDashboards {
-      id
-      luid
-    }
-    upstreamDatasources {
-      id
-      name
-    }
-  }
-
-  publishedDatasources {
-    id
-    luid
-    name
-    description
-    hasUserReference
-    hasExtracts
-    extractLastRefreshTime
-    extractLastIncrementalUpdateTime
-    extractLastUpdateTime
-    projectName
-    projectVizportalUrlId
-    isCertified
-    certificationNote
-    certifierDisplayName
-    containsUnsupportedCustomSql
-    datasourceFilters {
-      id
-      field {
-        id
-      }
-    }
-    parameters {
-      id
-      name
-    }
-    hasActiveWarning
-    dataQualityWarnings {
-      id
-      luid
-      isActive
-      isElevated
-      value
-      category
-      message
-      createdAt
-      updatedAt
-      asset {
-        id
-        luid
-        name
-      }
-    }
-    dataQualityCertifications {
-      id
-      luid
-      isActive
-      isElevated
-      value
-      category
-      message
-      createdAt
-      updatedAt
-      asset {
-        id
-        luid
-        name
-      }
-    }
-    labels {
-      id
-      luid
-      isActive
-      isElevated
-      value
-      category
-      message
-      createdAt
-      updatedAt
-      asset {
-        id
-        luid
-        name
-      }
-    }
-    tags {
-      id
-      name
-    }
-    createdAt
-    updatedAt
-    downstreamWorkbooks {
-      id
-      luid
-    }
-  }
-}""",
+        "query": query,
         "variables": {}
     })
 
     headers = {
         'Content-Type': 'application/json',
-        'X-Tableau-Auth': 'WI9l0DTRTh2aumfGuZW8SQ|apfva4G3v5AOqiL99x1k7HK8NUciNrSD|87db4137-fce1-4392-ac84-a289b96e3373'
+        'Accept': 'application/json',
+        'X-Tableau-Auth': token
     }
 
     async with aiohttp.ClientSession() as session:
