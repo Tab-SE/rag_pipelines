0e48b242fa749cba4957ea00b79c1a3d5e04afdd
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-22 22:50:59 -0600

    define catalog areas of interest

--- libs/metadata.py
+++ libs/metadata.py
@@ -5,10 +5,13 @@ from jsonpath_ng import jsonpath, parse
 from utils import http
 
 async def query_workbooks(credentials):
+    print(f'Workbooks metadata received')
     pass
 
 async def query_views(credentials):
+    print(f'Views metadata received')
     pass
 
 async def query_datasources(credentials):
+    print(f'Datasources metadata received')
     pass
