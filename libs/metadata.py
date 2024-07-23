ee40f9703b46f483d3cc21132aa12927583bf75e
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-22 23:16:50 -0600

    separating queries from functions

--- libs/metadata.py
+++ libs/metadata.py
@@ -1,17 +1,176 @@
-import os
-
 from jsonpath_ng import jsonpath, parse
 
-from utils import http
+from utils import gql
 
-async def query_workbooks(credentials):
+async def query_workbooks(token):
     print(f'Workbooks metadata received')
-    pass
+    workbooks = gql.query(query=workbooks_query, token=token)
+    return workbooks
 
-async def query_views(credentials):
+async def query_views(token):
     print(f'Views metadata received')
-    pass
+    views = gql.query(query=views_query, token=token)
+    return views
 
-async def query_datasources(credentials):
+async def query_datasources(token):
     print(f'Datasources metadata received')
-    pass
+    datasources = gql.query(query=datasources_query, token=token)
+    return datasources
+
+workbooks_query = """query Workbooks {
+  workbooks {
+    id
+    luid
+    name
+    description
+    createdAt
+    projectName
+    projectVizportalUrlId
+    projectLuid
+    uri
+    tags {
+      id
+      name
+    }
+    dashboards {
+      id
+      luid
+    }
+    sheets {
+      id
+      luid
+    }
+  }
+}"""
+
+views_query = """query Views {
+  dashboards {
+    id
+    luid
+    name
+    path
+    createdAt
+    updatedAt
+    index
+    tags {
+      id
+      name
+    }
+    sheets {
+      id
+      luid
+    }
+  }
+
+  sheets {
+    id
+    luid
+    documentViewId
+    name
+    path
+    createdAt
+    updatedAt
+    index
+    tags {
+      id
+      name
+    }
+    containedInDashboards {
+      id
+      luid
+    }
+    upstreamDatasources {
+      id
+      name
+    }
+  }
+}"""
+
+datasources_query = """query datasources {
+  publishedDatasources {
+    id
+    luid
+    name
+    description
+    hasUserReference
+    hasExtracts
+    extractLastRefreshTime
+    extractLastIncrementalUpdateTime
+    extractLastUpdateTime
+    projectName
+    projectVizportalUrlId
+    isCertified
+    certificationNote
+    certifierDisplayName
+    containsUnsupportedCustomSql
+    datasourceFilters {
+      id
+      field {
+        id
+      }
+    }
+    parameters {
+      id
+      name
+    }
+    hasActiveWarning
+    dataQualityWarnings {
+      id
+      luid
+      isActive
+      isElevated
+      value
+      category
+      message
+      createdAt
+      updatedAt
+      asset {
+        id
+        luid
+        name
+      }
+    }
+    dataQualityCertifications {
+      id
+      luid
+      isActive
+      isElevated
+      value
+      category
+      message
+      createdAt
+      updatedAt
+      asset {
+        id
+        luid
+        name
+      }
+    }
+    labels {
+      id
+      luid
+      isActive
+      isElevated
+      value
+      category
+      message
+      createdAt
+      updatedAt
+      asset {
+        id
+        luid
+        name
+      }
+    }
+    tags {
+      id
+      name
+    }
+    createdAt
+    updatedAt
+    downstreamWorkbooks {
+      id
+      luid
+    }
+  }
+}"""
